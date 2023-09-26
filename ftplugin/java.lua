7536179a76f61513c2f33a47ddc6327e70554652
Holopainen Tapio <ext-tapio.holopainen@kela.fi>
2023-09-26 09:25:48 +0300

    listchars + changes to java lsp

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,44 +1,44 @@
-local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-local home = vim.fn.expand '$HOME'
-local workspace_dir = home .. '.cache/jdtls/workspace/' .. project_name
-local jdlts_path = vim.fn.expand '$HOME' .. '/.local/share/kickstart.nvim/mason/packages/jdtls/'
-local launcher_version = '1.6.400.v20210924-0641'
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local home = vim.fn.expand("$HOME")
+local workspace_dir = home .. ".cache/jdtls/workspace/" .. project_name
+local jdlts_path = vim.fn.expand("$HOME") .. "/.local/share/lazyvim.nvim/mason/packages/jdtls/"
+local launcher_version = "1.6.400.v20210924-0641"
 -- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
 local config = {
   -- The command that starts the language server
   -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
   cmd = {
 
-    'java', -- or '/path/to/java17_or_newer/bin/java'
+    "java", -- or '/path/to/java17_or_newer/bin/java'
     -- depends on if `java` is in your $PATH env variable and if it points to the right version.
 
-    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-    '-Dosgi.bundles.defaultStartLevel=4',
-    '-Declipse.product=org.eclipse.jdt.ls.core.product',
-    '-Dlog.protocol=true',
-    '-Dlog.level=ALL',
-    '-Xmx1g',
-    '--add-modules=ALL-SYSTEM',
-    '--add-opens',
-    'java.base/java.util=ALL-UNNAMED',
-    '--add-opens',
-    'java.base/java.lang=ALL-UNNAMED',
+    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+    "-Dosgi.bundles.defaultStartLevel=4",
+    "-Declipse.product=org.eclipse.jdt.ls.core.product",
+    "-Dlog.protocol=true",
+    "-Dlog.level=ALL",
+    "-Xmx1g",
+    "--add-modules=ALL-SYSTEM",
+    "--add-opens",
+    "java.base/java.util=ALL-UNNAMED",
+    "--add-opens",
+    "java.base/java.lang=ALL-UNNAMED",
 
-    '-jar',
-    jdlts_path .. 'plugins/org.eclipse.equinox.launcher_' .. launcher_version .. '.jar',
+    "-jar",
+    jdlts_path .. "plugins/org.eclipse.equinox.launcher_" .. launcher_version .. ".jar",
 
-    '-configuration',
-    jdlts_path .. 'config_linux',
+    "-configuration",
+    jdlts_path .. "config_linux",
 
     -- 💀
     -- See `data directory configuration` section in the README
-    '-data',
+    "-data",
     workspace_dir,
   },
 
   -- This is the default if not provided, you can remove it. Or adjust as needed.
   -- One dedicated LSP server & client will be started per unique root_dir
-  root_dir = require('jdtls.setup').find_root { '.git', 'mvnw', 'gradlew' },
+  root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew" }),
 
   -- Here you can configure eclipse.jdt.ls specific settings
   -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
@@ -60,4 +60,4 @@ local config = {
 }
 -- This starts a new client & server,
 -- or attaches to an existing client & server depending on the `root_dir`.
-require('jdtls').start_or_attach(config)
+require("jdtls").start_or_attach(config)
